6c9368c0363d1cd5912036d36a010cfb80fa0d1a
Lari Hotari <lari@hotari.net>
2021-06-02 07:43:32 +0300

    Add scripts for updating docker image tag in custom_values.yaml

---
+++ helm-deployment/update_java_test_image_tag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# requires yq, https://mikefarah.gitbook.io/yq/
+# https://mikefarah.gitbook.io/yq/operators/assign-update#updated-multiple-paths
+cd $SCRIPT_DIR
+./update_image_tag.sh "${1:-$(docker images localhost:32000/apachepulsar/java-test-image --format '{{ .Tag }}' | head -n 1)}" "${2:-custom_values.yaml}"
